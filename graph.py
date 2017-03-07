d5a65ef3aaba7ae5b280d47b80be89f509a7c543
Heldiggris <potenkog@gmail.com>
2017-03-07 22:31:15 +0300

    2: Основы

--- graph.py
+++ graph.py
@@ -1,11 +1,18 @@
 import pylab
 from networkx import nx
 import matplotlib.pyplot as plt
+import networkx.algorithms.clique as cl
+
+'''
+Программа принимает матрицу смежности, строит на её основе граф,
+находит максимальную клику, рисует граф, раскрашивая вершины, входящие в максимальную клику,
+в зелёный цвет, а остальные в красный
+'''
 
 def Graf_create(matrix):
     '''
     Принимет матрицу связности и на её основе строит матрицу,
-    возвращает построенные граф
+    возвращает построенный граф
     '''
     graph = nx.Graph()
     graph.add_nodes_from(range(len(matrix)))
@@ -15,13 +22,35 @@ def Graf_create(matrix):
                 graph.add_edge(i, j)
     return graph
 
+def Graf_max_clique(graph):
+    '''
+    Принимает граф и возвращает максимальную клику
+    '''
+    clique_max = []
+    clique = [i for i in cl.find_cliques(graph)]
+    max_len_clique = len(clique[0])
+    for i in clique:
+        print(i)
+        if(max_len_clique <= len(i)):
+            print(len(i))
+            max_len_clique = len(i)
+            clique_max = i
+    return clique_max
+
 if __name__ == '__main__':
-    graph_1 = nx.Graph()
     graph_matrix = []
     a = list(map(int, input().split()))
     graph_matrix.append(a)
     for i in range(len(a) - 1):
         a = list(map(int, input().split()))
         graph_matrix.append(a)
-    nx.draw_shell(Graf_create(graph_matrix))
+    graph = Graf_create(graph_matrix)
+    clique_max = Graf_max_clique(graph)
+    color=[]
+    for i in range(len(a) * len(a) - 1):
+        if i in clique_max:
+            color.append('g')
+        else:
+            color.append('r')
+    nx.draw_shell(graph, node_color=color,width=2, with_labels=True)
     pylab.show()
