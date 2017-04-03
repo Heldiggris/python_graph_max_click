06a85f5868bb138df6b56add9d75d9b0b0e4141d
Heldiggris <potenkog@gmail.com>
2017-04-04 00:06:08 +0300

    ПОлная замена программы

---
+++ app/views.py
@@ -0,0 +1,36 @@
+from flask import render_template, flash, redirect
+from app import app
+# from .forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+#     user = { 'nickname': 'Макс' } # выдуманный пользователь
+#     posts = [ # список выдуманных постов
+#         { 
+#             'author': { 'nickname': 'John' }, 
+#             'body': 'Beautiful day in Portland!' 
+#         },
+#         { 
+#             'author': { 'nickname': 'Susan' }, 
+#             'body': 'The Avengers movie was so cool!' 
+#         },
+#         { 
+#             'author': { 'nickname': 'Boris' }, 
+#             'body': 'I am not Boris!' 
+#         }
+#     ]
+    return render_template("beta.html")
+@app.route('/cytoscape-edge.js')
+def login():
+    return render_template("cytoscape-edge.js")
+# @app.route('/login', methods = ['GET', 'POST'])
+# def login():
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+#         return redirect('/index')
+#     return render_template('login.html', 
+#         title = 'Sign In',
+#         form = form,
+#         providers = app.config['OPENID_PROVIDERS'])
